4838e5ce1688203fda31449d5aaae5a62c8d8b1e
sara nazemi <nazemi.sr@gmail.com>
2025-04-07 11:22:58 +0330

    add SecurityAspect

---
+++ src/main/java/com/example/springaop/aspects/SecurityAspect.java
@@ -0,0 +1,26 @@
+package com.example.springaop.aspects;
+
+
+import com.example.springaop.annotations.RequiresRole;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class SecurityAspect {
+
+    public void checkAccess(RequiresRole requiresRole){
+        String requiredRole = requiresRole.value();
+        String currentUserRole = getCurrentUserRole();
+        if(!requiredRole.equals(currentUserRole)){
+           throw new SecurityException("Access Denied. Required role : " + requiredRole);
+        }
+
+    }
+
+    private  String getCurrentUserRole(){
+        //شبیه سازی نقش کاربر جاری
+        return "user"; // or ADMIN
+    }
+
+}
